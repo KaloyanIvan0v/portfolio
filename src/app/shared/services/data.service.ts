84a35d8defbfeb50b31f4792444976125a1f9ebb
Kaloyan Ivanov <kaloyanivanov@MacBook-Air-von-Kaloyan.local>
2024-09-20 20:50:10 +0200

    style: add shadows to every section

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -8,6 +8,8 @@ export class DataService {
 
   projectsData = [
     {
+      liveTestUrl: 'https://join.kaloyanivanov.de',
+      gitHubUrl: 'https://github.com/KaloyanIvan0v/Join',
       imgPath: './../../../assets/img/03-portfolio-section/join-screenshot.png',
       name: 'Join',
       techStack: ['HTML', 'CSS', 'JavaScript', 'Firebase'],
@@ -15,6 +17,9 @@ export class DataService {
         'Join is a web application that allows users to easily create and join events.',
     },
     {
+      liveTestUrl: 'https://pollys-adventure.kaloyanivanov.de/',
+      gitHubUrl:
+        'https://github.com/KaloyanIvan0v/JumpAndRun-Polly-s-Adventure-.git',
       imgPath:
         './../../../assets/img/03-portfolio-section/pollys-adventure-screenshot.png',
       name: 'Polys Adventure',
@@ -23,6 +28,8 @@ export class DataService {
         'Join is a web application that allows users to easily create and join events.',
     },
     {
+      liveTestUrl: '',
+      gitHubUrl: '',
       imgPath:
         './../../../assets/img/03-portfolio-section/application-view.png',
       name: 'DA Bubble',
