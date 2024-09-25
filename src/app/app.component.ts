2b0108547f6db0e673daadcd44a90eed93b5f564
Kaloyan Ivanov <kaloyanivanov@web.de>
2024-09-25 19:13:19 +0200

    style: added aos animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+
+  ngOnInit() {
+    AOS.init({
+      offset: 0,
+      duration: 450,
+      easing: 'ease-in-out',
+      delay: 20,
+      once: true,
+    });
+  }
 }
