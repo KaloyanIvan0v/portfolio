bfb0e72756a811df4f8137ef6050f8a52627550a
Kaloyan Ivanov <95740404+KaloyanIvan0v@users.noreply.github.com>
2025-01-28 08:20:19 +0100

    fix: project description order

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -23,13 +23,23 @@ export class DataService {
 
   projectsData = [
     {
+      liveTestUrl: 'https://da-bubble.kaloyanivanov.de',
+      gitHubUrl: 'https://github.com/KaloyanIvan0v/daBubble.git',
+      imgPath:
+        './../../../assets/img/03-portfolio-section/dabubble-screenshot.png',
+      name: 'DA Bubble',
+      techStack: ['Angular', 'TypeScript', 'Firebase'],
+      description: 'example',
+      //description is safed in assets/i18n/en.json and assets/i18n/de.json
+    },
+    {
       liveTestUrl: 'https://join.kaloyanivanov.de',
       gitHubUrl: 'https://github.com/KaloyanIvan0v/Join',
       imgPath: './../../../assets/img/03-portfolio-section/join-screenshot.png',
       name: 'Join',
       techStack: ['HTML', 'CSS', 'JavaScript', 'Firebase'],
-      description:
-        'Join is a web application that allows users to easily create and join events.',
+      description: 'example',
+      //description is safed in assets/i18n/en.json and assets/i18n/de.json
     },
     {
       liveTestUrl: 'https://pollys-adventure.kaloyanivanov.de/',
@@ -39,17 +49,8 @@ export class DataService {
         './../../../assets/img/03-portfolio-section/pollys-adventure-screenshot.png',
       name: 'Pollys Adventure',
       techStack: ['HTML', 'CSS', 'JavaScript'],
-      description:
-        'Join is a web application that allows users to easily create and join events.',
-    },
-    {
-      liveTestUrl: '',
-      gitHubUrl: '',
-      imgPath: './../../../assets/img/03-portfolio-section/join-screenshot.png',
-      name: 'DA Bubble',
-      techStack: ['Angular', 'TypeScript', 'Firebase'],
-      description:
-        'Join is a web application that allows users to easily create and join events.',
+      description: 'example',
+      //description is safed in assets/i18n/en.json and assets/i18n/de.json
     },
   ];
 }
